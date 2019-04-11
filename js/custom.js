c5b5c19375afbcde56f0baf109c1844aee97acf3
ankit <ankitkr5858@gmail.com>
2019-04-11 16:22:19 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+ $('[name="graduate"]').change(function(){
+            if($('[name="graduate"]:checked').is(":checked")){
+                $('.ug').hide();
+                $('.phd').show();
+            }else{
+                $('.ug').show();
+                $('.phd').hide();
+            }
+        });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+
+})
+})
+
